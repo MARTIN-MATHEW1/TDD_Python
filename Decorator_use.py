898b60e58a805b77d321151e0820898487712795
MARTIN MATHEW <73994021+MARTIN-MATHEW1@users.noreply.github.com>
2023-07-27 17:01:00 -0400

    corrected code

--- Decorator_use.py
+++ Decorator_use.py
@@ -1,7 +1,6 @@
 import operator
 
 
-
 def Age_sorter(func):
         def sort_on_age(inp_list):
              return (map(func,sorted(inp_list, key=operator.itemgetter(3))))
@@ -9,17 +8,24 @@ def Age_sorter(func):
 
 @Age_sorter
 def name_Format(Name_Details_list):
-      
-    return (('Mr' if Name_Details_list[2] == 'M' else 'Mrs') 
+    intermediate_result = (('Mr.' if Name_Details_list[2] == 'M' else 'Mrs.') 
             + Name_Details_list[0]
             + " " 
             + Name_Details_list[1])
-
-
+    # print(intermediate_result)
+    return (intermediate_result)
 
 def Name_and_Details(inp_list):
-    return (*name_Format(inp_list))
+    return list(name_Format(inp_list))
 
+# details_list = [
+#     ["John", "Doe", "M", 30],
+#     ["Jane", "Smith", "F", 25],
+#     ["Michael", "Johnson", "M", 40]
+# ]
 
+# formatted_names = Name_and_Details(details_list)
+# for name in formatted_names:
+#     print(name)
 
 
--- test_decorator.py
+++ test_decorator.py
@@ -3,9 +3,10 @@ import Decorator_use
 
 class Decorator_Test(unittest.TestCase):
     def test_sorted_names_with_age(self):
-        print("hi")
-        expected = ["Mr. James Dani", "Mr. Martin Mathew", "Mrs. Alice Ben"]
-        input_name_and_details = [["James","Dani", "M", 30], ["Martin","Mathew", "M", 25], ["Alice","Ben", "F", 18]]
+        expected = [ "Mrs.Alice Ben","Mr.Martin Mathew","Mr.James Dani"]
+        input_name_and_details = [["James","Dani", "M", 30], 
+                                  ["Martin","Mathew", "M", 25], 
+                                  ["Alice","Ben", "F", 18]]
         Result = Decorator_use.Name_and_Details(input_name_and_details)
         self.assertEqual(expected, Result)
 
