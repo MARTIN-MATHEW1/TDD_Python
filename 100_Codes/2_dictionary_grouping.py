4aee7bbe1e6aee87dd3db9a7b0038f9a3c2a1aa8
MARTIN MATHEW <73994021+MARTIN-MATHEW1@users.noreply.github.com>
2023-08-12 18:42:39 -0400

    2

--- 100_Codes/2_dictionary_grouping.py
+++ 100_Codes/2_dictionary_grouping.py
@@ -7,17 +7,24 @@
 
 import unittest
 
+def list_grping(lst):
+    result_dict= {}
+    for k,v in lst: 
+        result_dict.setdefault(k, []).append(v)
+
+
+        print(result_dict)
+    return result_dict
 
 
 class dict_test(unittest.TestCase):
+
     def test_make_dict(self):
         Original_list = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
         expected_dict = {'yellow': [1, 3], 'blue': [2, 4], 'red': [1]}
-
-
-
+        self.assertEqual(expected_dict, list_grping(Original_list))
 
 
 if __name__ == "__main__":
-    unittest.main
+    unittest.main()
 
